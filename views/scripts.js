eb59bf785e16be57568cf8f3a2df8cd656dee0a4
lindokuhle001 <mlixshaba@gmail.com>
2022-04-16 02:42:39 +0200

    styled forms

--- views/scripts.js
+++ views/scripts.js
@@ -3,8 +3,29 @@ const modalOverlay = document.getElementsByClassName("modal-overlay")[0];
 const toggleModal = document.getElementsByClassName("modal-btn");
 
 for (const btn of toggleModal) {
-    console.log("hi");
   btn.addEventListener("click", () => {
+    if (!Array.from(projectDetailsForm.classList).includes("hide")) {
+      projectDetailsForm.classList.toggle("hide");
+    }
+
+    clientDetailsForm.classList.remove("hide");
     modalOverlay.classList.toggle("hide");
   });
 }
+
+// get the next form on modal
+const projectDetailsBtn = document.getElementById("projectDetailsBtn");
+const clientDetailsForm = document.getElementById("clientDetailsForm");
+const projectDetailsForm = document.getElementById("projectDetailsForm");
+projectDetailsBtn.addEventListener("click", () => {
+  clientDetailsForm.classList.toggle("hide");
+  projectDetailsForm.classList.toggle("hide");
+});
+
+// get the previous form on modal
+const backToClientFormBtn = document.getElementById("backToClientFormBtn");
+
+backToClientFormBtn.addEventListener("click", () => {
+  clientDetailsForm.classList.toggle("hide");
+  projectDetailsForm.classList.toggle("hide");
+});
