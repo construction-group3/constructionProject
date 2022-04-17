a4b74fad98620576c45a1df665c6f3fc30ef23f3
lindokuhle001 <mlixshaba@gmail.com>
2022-04-18 01:45:45 +0200

    added queries

--- src/connectDB.js
+++ src/connectDB.js
@@ -1,19 +1,135 @@
 require("dotenv").config();
 const sql = require("mssql");
-const {sqlConfig} = require("./sqlConfig")
+const { sqlConfig } = require("./sqlConfig");
 
-const getRecords = async () => {
+// CHECKING A PARTICULAR PROJECT USING A PROJECT ID
+const getProject = async (ID) => {
   try {
     const pool = await sql.connect(sqlConfig);
 
-    let result1 = await pool.request()
-    .input('input_parameter', sql.Int, 3)
-    .query(`select * from projects where projectID = @input_parameter`)
+    let result1 = await pool
+      .request()
+      .input("input_parameter", sql.Int, ID)
+      .query(
+        `SELECT ProjectID,ClientID,TeamID,HouseID, ProjectLocation,AmountPaidInZAR,BalanceInZAR,StatusType FROM IDFilter(@input_parameter) `
+      );
     console.log(result1.recordset);
   } catch (err) {
     throw err;
   }
-
 };
 
-getRecords();
+// CHECK PROGRESS USING PROJECT ID
+const checkProgress = async (ID) => {
+  try {
+    const pool = await sql.connect(sqlConfig);
+
+    let result1 = await pool
+      .request()
+      .input("input_parameter", sql.Int, ID)
+      .query(
+        `SELECT ProjectID,FirstName,LastName,HouseType,EstimatedDurationInMonths,teamName, ProjectLocation,AmountPaidInZAR,BalanceInZAR,StatusType FROM StatusFilter(1)    `
+      );
+    console.log(result1.recordset);
+  } catch (err) {
+    throw err;
+  }
+};
+
+// -- CHECKING THE COMPLETED TEAMS
+const getCompletedProjects = async (statusID) => {
+  try {
+    const pool = await sql.connect(sqlConfig);
+
+    let result1 = await pool
+      .request()
+      .input("input_parameter", sql.Int, statusID)
+      .query(
+        `SELECT ProjectID,FirstName,LastName,HouseType,EstimatedDurationInMonths,teamName, ProjectLocation,AmountPaidInZAR,BalanceInZAR,StatusType FROM StatusFilter(2)
+        `
+      );
+    console.log(result1.recordset);
+  } catch (err) {
+    throw err;
+  }
+};
+
+// SHOW AVAILABLE TEAMS
+const getAvailableTeams = async () => {
+  try {
+    const pool = await sql.connect(sqlConfig);
+
+    let result1 = await pool
+      .request()
+      .query(`SELECT TeamID,TeamName FROM AvalableTeams`);
+    console.log(result1.recordset);
+  } catch (err) {
+    throw err;
+  }
+};
+
+// -- SHOWS ALL THE PROJECTS
+const getAllProjects = async () => {
+  try {
+    const pool = await sql.connect(sqlConfig);
+
+    let result1 = await pool.request().query(
+      `SELECT ProjectID,FirstName,LastName,HouseType,EstimatedDurationInMonths,teamName, ProjectLocation,AmountPaidInZAR,BalanceInZAR,StatusType FROM vw_AllProjects
+        `
+    );
+    console.log(result1.recordset);
+  } catch (err) {
+    throw err;
+  }
+};
+
+// SHOWS INVOICES USING THE CLIENT ID
+const getInvoice = async (clientID) => {
+  try {
+    const pool = await sql.connect(sqlConfig);
+
+    let result1 = await pool
+      .request()
+      .input("input_parameter", sql.Int, clientID)
+      .query(
+        `SELECT Title,FirstName,LastName,HouseType,AmountPaidInZAR,BalanceInZAR,StatusDescr FROM udf_Invoices(2)
+        `
+      );
+    console.log(result1.recordset);
+  } catch (err) {
+    throw err;
+  }
+};
+
+getInvoice(1)
+// ADD NEW PROJECT
+const para = [
+  { name: "TitleID", type: sql.Int, value: 1 },
+  { name: "FirstName", type: sql.NVarChar(250), value: "me" },
+  { name: "LastName", type: sql.NVarChar(250), value: "sirname" },
+  { name: "PhyicalAddress", type: sql.NVarChar(500), value: "home" },
+  {
+    name: "EmailAddress",
+    type: sql.NVarChar(250),
+    value: "mecomputer.com",
+  },
+  { name: "PhoneNumber", type: sql.VarChar(10), value: "1234567890" },
+  { name: "TeamID", type: sql.Int, value: "1" },
+  { name: "HouseID", type: sql.Int, value: "1" },
+  { name: "ProjectLocation", type: sql.NVarChar(500), value: "my home" },
+  { name: "Deposit", type: sql.Money, value: 50000 },
+];
+const addNewProject = async (params, procedure) => {
+  try {
+    let pool = await sql.connect(sqlConfig);
+
+    const result1 = await pool.request();
+    params.forEach(function (param) {
+      result1.input(param.name, param.type, param.value);
+      console.log(result1);
+    });
+    return await result1.execute(procedure);
+  } catch (err) {
+    throw err;
+  }
+};
