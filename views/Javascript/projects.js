97c30236ef4ef85bc57ca645266d5847889973f5
lindokuhle001 <mlixshaba@gmail.com>
2022-04-23 17:47:50 +0200

    styled projects

--- views/Javascript/projects.js
+++ views/Javascript/projects.js
@@ -19,16 +19,26 @@
 //     addTableRow(user);
 //   }
 // }
+
+
+// use object for urls
+fetch("http://localhost:3000/view-projects")
+  .then((res) => res.json())
+  .then((data) => {
+    data.reverse().forEach((project) => {
+      displayProject(projectsList, project);
+    });
+  });
+
 const projectsList = document.getElementById("projectsList");
-// console.log(projectsList);
 
 // use destructuring
 const displayProject = (projectsList, projectObj) => {
-//   console.log(projectObj);
-//   console.log(projectsList);
   const project = document.createElement("li");
   const table = document.createElement("table");
   table.setAttribute("id", `project${projectObj.projectID}`);
+
+// table scope
   for (const property in projectObj) {
     fillTableRow(property, table, projectObj);
   }
@@ -37,14 +47,15 @@ const displayProject = (projectsList, projectObj) => {
   projectsList.insertAdjacentElement("beforeend", project);
 };
 
+// 
 const fillTableRow = (property, table, projectObj) => {
-  let row = document.createElement("tr");
+  const row = document.createElement("tr");
 
-  let projectProperty = document.createElement("th");
+  const projectProperty = document.createElement("th");
   const propertyName = document.createTextNode(property);
   projectProperty.appendChild(propertyName);
 
-  let projectInfo = document.createElement("th");
+  const projectInfo = document.createElement("th");
   const propertyValue = document.createTextNode(projectObj[property]);
   projectInfo.appendChild(propertyValue);
 
@@ -54,12 +65,12 @@ const fillTableRow = (property, table, projectObj) => {
   table.insertAdjacentElement("beforeend", row);
 };
 
-const testData = {
-  name: "lindo",
-  age: 200,
-  height: 1.72,
-  weight: "65kg",
-  projectID: 69,
-};
+// const testData = {
+//   name: "lindo",
+//   age: 200,
+//   height: 1.72,
+//   weight: "65kg",
+//   projectID: 69,
+// };
 
-displayProject(projectsList, testData);
+// displayProject(projectsList, testData);
