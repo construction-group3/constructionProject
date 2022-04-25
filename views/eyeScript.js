345503c8ed9e47d899003aa45bc90ce10ca67a2f
lindokuhle001 <mlixshaba@gmail.com>
2022-04-25 08:28:39 +0200

    fixed login js

--- views/eyeScript.js
+++ views/eyeScript.js
@@ -3,15 +3,12 @@ const visibilityBtn = document.getElementById("visibilityBtn");
   visibilityBtn.addEventListener('click', toogleVisibility) 
   
   function toogleVisibility(){
-    const password = document.getElementById("password");
-
-    const icon = document.getElementById("icon")
     if(password.type === "password"){
         password.type = "text"
-        icon.innerText = "visibility_off"
+        visibilityBtn.innerText = "visibility_off"
     }
     else{
         password.type = "password"
-        icon.innerText = "visibility"
+        visibilityBtn.innerText = "visibility"
     }
 } 
